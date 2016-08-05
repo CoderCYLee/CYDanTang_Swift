d6b30ce5c9b112b4feab86bb52d50f813317297e
Cyrill <lichunyang@outlook.com>
2016-08-05 13:41:34 +0800

    初始化项目

---
+++ CYDanTang/CYDanTang/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CYDanTang
+//
+//  Created by Cyrill on 16/8/5.
+//  Copyright © 2016年 Cyrill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
